dd94cba73af537574a4e9a404b6833e99a771b9d
Muayad-Ayoub <muayadayoub@gmail.com>
2026-02-02 02:18:23 +0300

    Variable Sized Array from vs code

---
+++ Variable Sized Arrays.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n, q, k, row, column;
+    cin>>n>>q;
+    int **arr = new int*[n];
+    for(int i = 0; i<n;i++)
+    {
+        cin>>k;
+        arr[i] = new int[k];
+        for(int j=0;j<k;j++)
+        cin>>arr[i][j];        
+    }
+    for(int i=0;i<q;i++)
+    {
+        cin>>row>>column;
+        cout<<arr[row][column]<<endl;
+    }
+return 0;
+}
